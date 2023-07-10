cbef6e1592d99ba10bc78b625606f34c0487f500
Robert Daniels <rob.daniels3@gmail.com>
2023-07-11 00:25:44 +0100

    Implemented Ball class

--- libraries/Ball.js
+++ libraries/Ball.js
@@ -7,6 +7,8 @@ class Ball {
     this.speedY = random(-3, 3);
     this.x = random(100, w - 100);
     this.y = random(100, h - 100);
+    this.w = w;
+    this.h = h;
   }
 
   getRandomColour() {
@@ -17,9 +19,9 @@ class Ball {
   }
 
   display() {
-    fill(colour);
+    fill(this.colour);
     noStroke();
-    ellipse(x, y, radius, radius);
+    ellipse(this.x, this.y, this.radius, this.radius);
   }
 
   move() {
@@ -28,14 +30,14 @@ class Ball {
   }
 
   boundsCheck(w, h) {
-    if (this.x >= w - this.diameter) {
+    if (this.x >= this.w - this.diameter) {
       this.speedX *= -1;
     }
     if (this.x <= this.diameter) {
       this.speedX *= -1;
     }
-    if (this.y >= h - this.diameter) {
-      speedY *= -1;
+    if (this.y >= this.h - this.diameter) {
+      this.speedY *= -1;
     }
     if (this.y < this.diameter) {
       this.speedY *= -1;
