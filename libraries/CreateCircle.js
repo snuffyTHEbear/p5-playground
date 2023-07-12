b09b1886f7eb1c3941fe1f576536cfe4e00dd8ad
Robert Daniels <rob.daniels3@gmail.com>
2023-07-12 22:57:52 +0100

    Renamed Ball to CreateCircle

--- libraries/CreateCircle.js
+++ libraries/CreateCircle.js
@@ -1,10 +1,11 @@
-class Ball {
-  constructor(w, h) {
-    this.radius = random(20, 50);
-    this.diameter = this.radius * 0.5;
-    this.colour = this.getRandomColour();
-    this.speedX = random(-3, 3);
-    this.speedY = random(-3, 3);
+class CreateCircle {
+  constructor(w, h, c = 255) {
+    this.diameter = random(20, 50);
+    this.radius = this.diameter * 0.5;
+    //this.colour = this.getRandomColour();
+    this.colour = c;
+    this.speedX = random(6);
+    this.speedY = random(5);
     this.x = random(100, w - 100);
     this.y = random(100, h - 100);
     this.w = w;
@@ -21,7 +22,7 @@ class Ball {
   display() {
     fill(this.colour);
     noStroke();
-    ellipse(this.x, this.y, this.radius, this.radius);
+    ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 
   move() {
@@ -29,18 +30,20 @@ class Ball {
     this.y += this.speedY;
   }
 
-  boundsCheck(w, h) {
-    if (this.x >= this.w - this.diameter) {
+  boundsCheck() {
+    if (this.x >= this.w - this.radius) {
       this.speedX *= -1;
     }
-    if (this.x <= this.diameter) {
+    if (this.x <= this.radius) {
       this.speedX *= -1;
     }
-    if (this.y >= this.h - this.diameter) {
+    if (this.y >= this.h - this.radius) {
       this.speedY *= -1;
     }
-    if (this.y < this.diameter) {
+    if (this.y < this.radius) {
       this.speedY *= -1;
     }
   }
+
+  
 }
--- sketch.js
+++ sketch.js
@@ -1,25 +1,45 @@
-let w = 900;
-let h = 650;
-let centreX = w * 0.5;
-let centreY = h * 0.5;
+let centreX, centreY;
 /**
- * @type {Ball}
+ * 
  */
-let b;
-/**
- * @type {Box}
- */
-let box;
+let circles;
+
+let colours;
 
 function setup() 
 {
-  createCanvas(w, h);
-  print(degrees(PI/2));
-  
+  createCanvas(windowWidth, windowHeight);
+  this.centreX = windowWidth * 0.5;
+  this.centreY = windowHeight *0.5;
+  frameRate(60)
+
+  colours = [color(76, 75, 22), color(137, 129, 33), color(231, 177, 10), color(247, 241, 229)];
+
+  circles = [];
+
+  for(var i = 0; i < 10; i++)
+  {
+    var n = round(random(colours.length-1));
+    var c = colours[n];
+    circles.push(new CreateCircle(windowWidth, windowHeight, c));
+  }
 }
 
 function draw() 
 {
-  background(255);
+  background(0);
   
+  for(c of circles)
+  {
+    c.move();
+    c.boundsCheck();
+    c.display();
+  }
+}
+
+function windowResized()
+{
+  createCanvas(windowWidth, windowHeight);
+  this.centreX = windowWidth * 0.5;
+  this.centreY = windowHeight *0.5;
 }
